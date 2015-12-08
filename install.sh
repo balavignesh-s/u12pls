4c40c7d4e5e98cc9c3fe87a7f663dc5c5fb2de96
spsiddarthan <siddharthan@shippable.com>
2015-12-08 16:52:46 +0530

    Updated 12.04 image with the fixes made to the 14.04 image.

--- install.sh
+++ install.sh
@@ -1,5 +1,20 @@
-#!/bin/bash
-for file in /tmp/version/*;
+#!/bin/bash -e
+
+mkdir -p /tmp
+chmod 1777 /tmp
+
+for file in /u12pls/version/*;
 do
-  $file
+  . $file
 done
+
+echo "================= Adding mysql cnf ==================="
+cd /u12pls && cp -rf my.cnf /etc/mysql/my.cnf
+
+echo "================= Adding PostgreSQL cnf ==================="
+cd /u12pls && cp -rf pg_hba.cnf /etc/postgresql/9.4/main/pg_hba.conf
+
+echo "================= Cleaning package lists ==================="
+apt-get clean
+apt-get autoclean
+apt-get autoremove
--- version/selenium_2_45.sh
+++ version/selenium_2_45.sh
@@ -1,27 +1,56 @@
 #!/bin/bash -e
 
-# Make sure Java is installed
-java_version=$(java -version 2>&1 | awk -F '"' '/version/ {print $2}')
-if [[ -z "$java_version" ]]; then
-  echo "ElasticSearch needs Java to be installed on your system. Exiting."
-  exit 1
-fi
-
-# Install prerequisites
+echo "================= Installing Selenium PreReqs ==================="
 sudo apt-get install -y xvfb firefox unzip
 
-# Install chrome
+sudo apt-get update && apt-get install -y \
+  ca-certificates \
+  gconf-service \
+  hicolor-icon-theme \
+  libappindicator1 \
+  libasound2 \
+  libcanberra-gtk-module \
+  libcurl3 \
+  libexif-dev \
+  libgconf-2-4 \
+  libgl1-mesa-dri \
+  libgl1-mesa-glx \
+  libnspr4 \
+  libnss3 \
+  libpango1.0-0 \
+  libv4l-0 \
+  libxss1 \
+  libxtst6 \
+  fonts-liberation\
+  xdg-utils \
+  --no-install-recommends
+
+echo "================= Installing Chrome ==================="
 sudo wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
 sudo apt-get install -y -f
 sudo rm -f google-chrome-stable_current_amd64.deb
 
-# Install chrome driver
+echo "================= Installing Chrome driver ==================="
 sudo wget http://chromedriver.storage.googleapis.com/2.13/chromedriver_linux64.zip
 sudo unzip chromedriver_linux64.zip && sudo rm -f chromedriver_linux64.zip
 sudo mv chromedriver /usr/local/bin/chromedriver
 sudo chmod a+x /usr/local/bin/chromedriver
 
-# Install Selenium
+sudo apt-get install firefox chromium-browser;
+sudo ln -s /usr/bin/chromium-browser /usr/bin/chrome;
+sudo apt-get -f install;
+
+wget -q -O- https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo apt-get update
+sudo apt-get install google-chrome-stable
+
+echo "================= Installing Opera ==================="
+wget -q -O-  http://deb.opera.com/archive.key | sudo apt-key add -
+sudo sh -c 'echo "deb http://deb.opera.com/opera/ stable non-free" > /etc/apt/sources.list.d/opera.list'
+sudo apt-get update
+sudo apt-get install -y opera
+
+echo "================= Installing Selenium ==================="
 sudo mkdir /usr/local/selenium >/dev/null 2>&1
 sudo wget http://selenium-release.storage.googleapis.com/2.45/selenium-server-standalone-2.45.0.jar -P /usr/local/selenium
