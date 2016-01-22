7ef9a8b82877123c2b514de205c47158cafe80f4
himanshu0503 <hemanshu0503@gmail.com>
2016-01-22 14:48:34 +0530

    Removes sh extension

--- version/mysql_5_6.sh
+++ version/mysql_5_6.sh
@@ -26,7 +26,7 @@ cp support-files/mysql.server /etc/init.d/mysql.server
 echo "=========== Installing mysql clients 5.6 ==============="
 wget http://repo.mysql.com/mysql-apt-config_0.3.3-1ubuntu12.04_all.deb
 apt-get install expect
-./installMySQLClient.sh
+./installMySQLClient
 apt-get update
 apt-get install mysql-client
 rm mysql-apt-config_0.3.3-1ubuntu12.04_all.deb
